98eba529fd3d1dde7536a5795c96536bb2dc2580
Roman031004 <rolozasan@gmail.com>
2025-01-04 11:20:25 -0500

    Descripción de los cambios

--- app.py
+++ app.py
@@ -2,16 +2,13 @@ from flask import Flask, render_template, request
 from models import db, Proyect
 from flask_migrate import Migrate
 
-# Crear la aplicación Flask
 app = Flask(__name__)
 
-# Configuración de la base de datos
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///portfolio.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Para evitar una advertencia de SQLAlchemy
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Inicializar la base de datos y las migraciones
 db.init_app(app)
-migrate = Migrate(app, db)  # Esta línea debe ir después de la creación de `app`
+migrate = Migrate(app, db)  
 
 @app.route('/proyect')
 def proyectos():
@@ -24,7 +21,7 @@ def contacto():
         nombre = request.form['nombre']
         email = request.form['email']
         mensaje = request.form['mensaje']
-        print(f'Mensaje de {nombre} ({email}): {mensaje}')  # Esto es solo para probar
+        print(f'Mensaje de {nombre} ({email}): {mensaje}')  
         return render_template('contact.html', mensaje_enviado=True)
     return render_template('contact.html', mensaje_enviado=False)
 
